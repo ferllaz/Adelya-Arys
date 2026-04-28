8b77c5ff9e4e6aab300bc3f41ec66cb9dcb43c10
gg666777228 <98387848+gg666777228@users.noreply.github.com>
2026-04-28 17:25:41 +0500

    add products api

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,13 +1,64 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Product
-from .serializers import ProductSerializer
+from django.http import JsonResponse
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.views.decorators.csrf import csrf_exempt
 
-# Список (GET) и создание (POST)
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from .serializers import ProductSerializer
+from .models import Product
+
+
+@csrf_exempt
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            return JsonResponse({
+                "message": "ok",
+                "user": user.username
+            })
+
+        return JsonResponse({"error": "invalid credentials"}, status=400)
+
+
+@csrf_exempt
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            request.session.save()
+
+            return JsonResponse({
+                "message": "register success",
+                "username": user.username,
+            }, status=201)
+
+        return JsonResponse({"errors": form.errors}, status=400)
+
+    return JsonResponse({"error": "only POST method allowed"}, status=405)
+
+
+@csrf_exempt
+def logout_view(request):
+    logout(request)
+    return JsonResponse({"message": "logout success"}, status=200)
+
+
+
 class ProductListCreateAPIView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-# Один товар (GET, PUT, DELETE)
 class ProductDetailAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
